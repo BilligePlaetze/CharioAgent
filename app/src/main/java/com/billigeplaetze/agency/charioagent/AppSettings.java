dd56566c17cb188048045e1d1d3ae229d754d31f
DannySE <d.seledtsova@gmail.com>
2018-03-24 20:30:57 +0100

    implemented login

---
+++ app/src/main/java/com/billigeplaetze/agency/charioagent/AppSettings.java
@@ -0,0 +1,16 @@
+package com.billigeplaetze.agency.charioagent;
+
+/**
+ */
+
+public class AppSettings {
+    public static String agentName;
+
+    public static String getAgentName() {
+        return agentName;
+    }
+
+    public static void setAgentName(String agentName) {
+        AppSettings.agentName = agentName;
+    }
+}
--- app/src/main/java/com/billigeplaetze/agency/charioagent/LoginActivity.java
+++ app/src/main/java/com/billigeplaetze/agency/charioagent/LoginActivity.java
@@ -1,13 +1,37 @@
 package com.billigeplaetze.agency.charioagent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.input_login)
+    EditText login;
+    @BindView(R.id.login_button)
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.login_button)
+    public void onLoginButtonClicked(View view){
+        String agentName = login.getText().toString();
+        AppSettings.setAgentName(agentName);
+
+        startActivity(new Intent(LoginActivity.this, QueueActivity.class));
+    }
+
+
+
 }
